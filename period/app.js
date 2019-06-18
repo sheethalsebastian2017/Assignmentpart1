caf77d71a1cf8e7269000489021f9efb2ffe3eca
sheethal sebastian <sheethalsebastian2017@gmail.com>
2019-06-17 22:19:38 -0400

    final commit

--- period/app.js
+++ period/app.js
@@ -3,20 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose = require('mongoose');
-mongoose.connect(
-  `mongodb+srv://Assignpart1:part123@assign1-o1cpz.mongodb.net/test?retryWrites=true&w=majority`,
-  {
-    useNewUrlParser: true
-  }
-); // Connect to mongo
-
-var db = mongoose.connection;
-db.on('error', err => console.error(err)); // Console log error on mongoose
-db.once('open', () => 
-  console.log('Connection to mongoose successful'));
-
-
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
